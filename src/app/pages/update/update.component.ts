dd33d89e1460a4d2c4b004c2a0bd37b55a298ab6
tuminhvo <tu.vo@gumiviet.com>
2022-12-13 17:21:36 +0700

    build: validate updateForm

--- src/app/pages/update/update.component.ts
+++ src/app/pages/update/update.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Todo } from 'src/app/todo';
 
+import { Todo } from 'src/app/todo';
 import { TodoService } from 'src/app/todo.service';
 
+// Validators
+import { noDuplicated } from 'src/app/validators/no-duplicated.validator';
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
@@ -13,6 +15,8 @@ import { TodoService } from 'src/app/todo.service';
 export class UpdateComponent implements OnInit {
   todos!: Todo[];
   todo!: Todo;
+
+  updateForm: any;
 
     constructor(
       private _activatedRoute: ActivatedRoute,
@@ -24,24 +28,28 @@ export class UpdateComponent implements OnInit {
   ngOnInit(): void {
     this.getTodo();
     this.elementRef.nativeElement.querySelector('input').focus();
+    this.updateForm = new FormGroup({
+      title: new FormControl(this.todo.title, [
+        Validators.pattern(/^[^-\s][a-zA-Z0-9_\s-]+$/),
+        noDuplicated(this.todos.filter(todo => todo != this.todo))
+      ])
+    });
   }
+
+  get title() { return this.updateForm.get('title') }
 
   getTodo(): void {
     let title = this._activatedRoute.snapshot.paramMap.get("title")!.split("-").join(" ");
     this.todos = this.$todoService.getTodos();
-    this.todo = this.todos.find(todo => todo.title = title)!;
+    this.todo = this.todos.find(todo => todo.title === title)!;
   }
 
   updateTitle() {
-    if (this.todo.title !== "") {
+    if (this.title.valid) {
       this.$todoService.putTodos(this.todos);
       this._route.navigateByUrl('/');
-    } else { // Delete todo if it's empty
-      this.$todoService.putTodos(
-        this.todos
-          .filter(todo => todo.title !== "")
-      );
-      this._route.navigateByUrl('/');
+    } else {
+      console.log("Invalid!");
     }
   }
 }
